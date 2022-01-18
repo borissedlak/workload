915a50253dd4be7f25e00a5e386dc53ad12b8fec
Boris Sedlak <bsedlak01@gmail.com>
2022-01-18 18:33:17 +0100

    newest tries

--- detector/main.py
+++ detector/main.py
@@ -4,4 +4,4 @@ detector = Detector(use_cuda=True, output_width=500, confidence_threshold=0.3)
 
 # detector.processImage("../server/demo/uaf.jpg")
 # detector.processVideo("../server/demo/lukas-detection.mp4") # FPS: CPU 10, GPU 32
-detector.processVideo("../server/demo/head-pose-detection.mp4")  # FPS: CPU 12, GPU 52
+detector.processVideo("../producer/demo_files/head-pose-detection.mp4")  # FPS: CPU 12, GPU 52

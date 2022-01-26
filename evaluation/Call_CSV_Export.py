8f607deb57540480706ee40fc060652a61715b93
Boris <bsedlak01@gmail.com>
2022-01-26 16:21:53 +0100

    with sleep

--- evaluation/Call_CSV_Export.py
+++ evaluation/Call_CSV_Export.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 
 sfu_url = "http://192.168.0.80:4000"
@@ -9,13 +11,15 @@ producer_get_stats = sfu_url + "/calculate_stats"
 producer_persist_stats = sfu_url + "/persist_stats"
 
 
-for i in range(20):
+for i in range(60):
     print(f"{i+1}. Round")
     consumer_ret = requests.get(sfu_get_stats, timeout=1.0).content
     producer_ret = requests.get(producer_get_stats, timeout=1.0).content
     print(consumer_ret)
     print(producer_ret)
     print("\n")
+    time.sleep(1)
+
 
 
 consumer_ret = requests.post(sfu_persist_stats, timeout=1.0).content
